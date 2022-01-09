dddf9a1dce8f279d72d797a1cc7e89a1abdbeac3
Tolga Yonca <ynctlg@gmail.com>
2022-01-09 05:43:26 +0300

    favicon added

--- src/components/CharacterCard/index.tsx
+++ src/components/CharacterCard/index.tsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { IThumbnail } from '../../types/CharactersType';
 
 interface ICharacter {
@@ -11,7 +10,7 @@ interface ICharacter {
 export const CharacterCard = ({ name, image, id }: ICharacter) => (
   <Link style={{ color: 'rgba(0,0,0,.85)' }} to={`/detail/${id}`}>
     <div className="character">
-      <LazyLoadImage className="character-img" src={`${image.path}.jpg`} alt="character" />
+      <img className="character-img" src={`${image.path}.jpg`} alt="character" />
       <div className="character-name">{name}</div>
     </div>
   </Link>
